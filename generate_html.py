0651b71610125b01bae756f6549913658748c180
Jaydeep Chakraborty (Student) <ASUAD\jchakra1@en4102988l.fulton.ad.asu.edu>
2017-04-10 17:18:26 -0700

    initial UI

---
+++ generate_html.py
@@ -0,0 +1,13 @@
+import webbrowser
+# f = open('graph.html','w')
+# 
+# msg = """<html>
+# <head></head>
+# <body><p>Hello World!</p></body>
+# </html>"""
+# 
+# f.write(msg)
+# f.close()
+
+filename = 'graph.html'
+webbrowser.open_new(filename)
