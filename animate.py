040ef264e54f1ec1d0d9e86451d723de55424229
Dhananjay <dvpandit@asu.edu>
2017-05-02 14:59:36 -0700

    Added colors for nodes during transition

--- animate.py
+++ animate.py
@@ -14,9 +14,12 @@ def plotGraph(red_edges):
     Edges = [('t1', 't2'), ('t1', 't3'), ('t2', 't4'), ('t3', 't5')]
     G.add_edges_from(Edges)
 
+    currentNode = ['t1']
     pos = nx.spring_layout(G, pos = initPosition, fixed = labels)
     black_edges = [edge for edge in G.edges()]
-    nx.draw_networkx_nodes(G,pos, node_color='c')
+    black_nodes = [edge[1] for edge in G.edges() if edge[1] not in currentNode]
+    nx.draw_networkx_nodes(G,pos, node_color='#C0C0C0')
+    nx.draw_networkx_nodes(G,pos, nodelist=currentNode, node_color='#00FF00')
     nx.draw_networkx_labels(G,pos)
     nx.draw_networkx_edges(G, pos, edgelist=black_edges,edge_color='r', arrows=True)
     plt.show()
@@ -24,9 +27,12 @@ def plotGraph(red_edges):
     currentList = []
     for edge in red_edges:
         currentList.append(edge)
+        currentNode.append(edge[1])
         pos = nx.spring_layout(G, pos = initPosition, fixed = labels)
         black_edges = [edge for edge in G.edges() if edge not in currentList]
-        nx.draw_networkx_nodes(G,pos, node_color='c')
+        black_nodes = [edge[1] for edge in G.edges() if edge[1] not in currentNode]
+        nx.draw_networkx_nodes(G,pos, nodelist=currentNode, node_color='#7CFC00')
+        nx.draw_networkx_nodes(G,pos, nodelist=black_nodes, node_color='#C0C0C0')
         nx.draw_networkx_labels(G,pos)
         nx.draw_networkx_edges(G, pos, edgelist=currentList, edge_color='b', arrows=True)
         nx.draw_networkx_edges(G, pos, edgelist=black_edges,edge_color='r', arrows=True)
